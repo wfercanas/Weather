95ade8ea01c2acf422c12d8c6f3b0dedf8242778
wfercanas <fernando.canas@outlook.com>
2021-06-02 23:27:10 -0500

    add/ file for all DOM Elements selection

---
+++ src/DOMElements.js
@@ -0,0 +1,6 @@
+/* All DOM Elements selection */
+const currentCityLabel = document.querySelector('.main-data__city');
+const currentTempLabel = document.querySelector('.main-data__temp');
+const currentFeelTempLabel = document.querySelector('.main-data__feelslike');
+
+export { currentCityLabel, currentTempLabel, currentFeelTempLabel };
