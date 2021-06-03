8de262ba144d2504d0f43367908ee7e7f9266ad1
wfercanas <fernando.canas@outlook.com>
2021-06-02 23:26:18 -0500

    add/ initial favorite cities data

---
+++ src/favoriteCities.js
@@ -0,0 +1,19 @@
+const favoriteCities = [
+  {
+    city: 'San Francisco',
+    lat: 37.7749,
+    lon: -122.4194,
+  },
+  {
+    city: 'Tokio',
+    lat: 35.6895,
+    lon: 139.6917,
+  },
+  {
+    city: 'Madrid',
+    lat: 40.4165,
+    lon: -3.7026,
+  },
+];
+
+export { favoriteCities };
