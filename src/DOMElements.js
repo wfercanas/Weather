809632be333ba8c454a591fafd3cf64f0cb7d9d4
wfercanas <fernando.canas@outlook.com>
2021-06-04 18:16:42 -0500

    add/ full home page elements

--- src/DOMElements.js
+++ src/DOMElements.js
@@ -1,6 +1,30 @@
 /* All DOM Elements selection */
+
+/* Main data */
 const currentCityLabel = document.querySelector('.main-data__city');
 const currentTempLabel = document.querySelector('.main-data__temp');
 const currentFeelTempLabel = document.querySelector('.main-data__feelslike');
 
-export { currentCityLabel, currentTempLabel, currentFeelTempLabel };
+/* Hourly values */
+const currentHourlyValues = document.querySelector('.hourly-data__content');
+
+/* Indicators value */
+const currentSunriseValue = document.getElementById('sunrise');
+const currentUvValue = document.getElementById('uv');
+const currentWindValue = document.getElementById('wind');
+const currentSunsetValue = document.getElementById('sunset');
+const currentPressureValue = document.getElementById('pressure');
+const currentHumidityValue = document.getElementById('humidity');
+
+export {
+  currentCityLabel,
+  currentTempLabel,
+  currentFeelTempLabel,
+  currentHourlyValues,
+  currentSunriseValue,
+  currentUvValue,
+  currentWindValue,
+  currentSunsetValue,
+  currentPressureValue,
+  currentHumidityValue,
+};
