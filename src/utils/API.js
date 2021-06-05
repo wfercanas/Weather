0e1c6dd2cdf36aa4b3f031aabe0b93bb2f8f9f7b
wfercanas <fernando.canas@outlook.com>
2021-06-05 13:36:48 -0500

    add/ API call when using city and country params

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,8 +1,13 @@
 const APIKeys = '9832905a1bed9547d41f2cdaed555ef3';
 const coordinatesAPI = 'https://api.openweathermap.org/data/2.5/onecall?';
+const cityAPI = 'https://api.openweathermap.org/data/2.5/weather?q=';
 
 const coordinatesURL = (lat, lon) => {
   return `${coordinatesAPI}lat=${lat}&lon=${lon}&exclude=minutely&units=metric&appid=${APIKeys}`;
 };
 
-export { coordinatesURL };
+const cityURL = (city, country) => {
+  return `${cityAPI}${city},${country}&units=metric&appid=${APIKeys}`;
+};
+
+export { coordinatesURL, cityURL };
