d77ddd3e65452631ece565a7c2c66f1710e444c6
wfercanas <fernando.canas@outlook.com>
2021-06-04 23:06:29 -0500

    add/ City Time returns object with date element

--- src/functions/timestampToCityTime.js
+++ src/functions/timestampToCityTime.js
@@ -1,10 +1,36 @@
+/* utils */
+import {
+  monthsOfTheYear,
+  dayweekNames,
+} from '../utils/monthsAndWeekdayNames.js';
+
 /* Return time in city time */
+const getDayOrdinal = (date) => {
+  switch (date) {
+    case '1':
+      return 'st';
+      break;
+    case '2':
+      return 'nd';
+      break;
+    case '3':
+      return 'rd';
+      break;
+    default:
+      return 'th';
+      break;
+  }
+};
+
 const timestampToCityTime = (timestamp, timezone) => {
   const localTime = new Date(timestamp * 1000);
   const cityTime = new Date(
     localTime.toLocaleString('en-US', { timeZone: timezone })
   );
   const cityTimeHourAndMinutes = {
+    date: `${dayweekNames[cityTime.getDay()]}, ${
+      monthsOfTheYear[cityTime.getMonth()]
+    } ${cityTime.getDate()}${getDayOrdinal(cityTime.getDate())}`,
     hour: `${
       cityTime.getHours() < 10 ? '0' + cityTime.getHours() : cityTime.getHours()
     }`,
