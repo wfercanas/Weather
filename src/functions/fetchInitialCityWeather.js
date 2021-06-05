3478ba78ef4ddaade0a966609af0fc7690a1c4aa
wfercanas <fernando.canas@outlook.com>
2021-06-05 18:45:08 -0500

    add/ function extracted from index.js

---
+++ src/functions/fetchInitialCityWeather.js
@@ -0,0 +1,30 @@
+/* Utils */
+import { coordinatesURL } from '../utils/API.js';
+import { favoriteCities } from '../utils/favoriteCities.js';
+
+/* Functions */
+import { setCurrentCityWeather } from './setCurrentCityWeather.js';
+import { setCurrentCityIndicators } from './setCurrentCityIndicators.js';
+import { setCurrentCityHourlyWeather } from './setCurrentCityHourlyWeather.js';
+import { setCurrentCityNextSevenDaysWeather } from './setCurrentCityNextSevenDaysWeather.js';
+import { setFavoriteCities } from './setFavoriteCities.js';
+
+const fetchInitialCityWeather = (city) => {
+  window
+    .fetch(coordinatesURL(city.lat, city.lon))
+    .then((response) => response.json())
+    .then((cityWeather) => {
+      /* Main Data */
+      setCurrentCityWeather(cityWeather, city.city);
+      /* Next 24 hours data */
+      setCurrentCityHourlyWeather(cityWeather);
+      /* Main Indicators */
+      setCurrentCityIndicators(cityWeather);
+      /* Next seven day forecast*/
+      setCurrentCityNextSevenDaysWeather(cityWeather);
+      /* Favorite cities cards*/
+      setFavoriteCities(favoriteCities);
+    });
+};
+
+export { fetchInitialCityWeather };
---
+++ src/functions/fetchNewCityWeather.js
@@ -0,0 +1,31 @@
+/* Utils */
+import { coordinatesURL } from '../utils/API.js';
+import { favoriteCities } from '../utils/favoriteCities.js';
+
+/* Functions */
+import { removeFavoriteCities } from './removeFavoriteCities.js';
+import { removePreviousCityHourlyWeather } from './removePreviousCityHourlyWeather.js';
+import { removePreviousCityNextSevenDaysWeather } from './removePreviousCityNextSevenDaysWeather.js';
+import { setCurrentCityWeather } from './setCurrentCityWeather.js';
+import { setCurrentCityIndicators } from './setCurrentCityIndicators.js';
+import { setCurrentCityHourlyWeather } from './setCurrentCityHourlyWeather.js';
+import { setCurrentCityNextSevenDaysWeather } from './setCurrentCityNextSevenDaysWeather.js';
+import { setFavoriteCities } from './setFavoriteCities.js';
+
+const fetchNewCityWeather = (city) => {
+  removePreviousCityHourlyWeather();
+  removePreviousCityNextSevenDaysWeather();
+  removeFavoriteCities();
+  window
+    .fetch(coordinatesURL(city.lat, city.lon))
+    .then((response) => response.json())
+    .then((cityWeather) => {
+      setCurrentCityWeather(cityWeather, city.city);
+      setCurrentCityHourlyWeather(cityWeather);
+      setCurrentCityIndicators(cityWeather);
+      setCurrentCityNextSevenDaysWeather(cityWeather);
+      setFavoriteCities(favoriteCities);
+    });
+};
+
+export { fetchNewCityWeather };
