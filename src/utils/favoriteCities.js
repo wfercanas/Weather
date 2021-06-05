e93ac4210049744fc5e8136d96d8d84ff285e200
wfercanas <fernando.canas@outlook.com>
2021-06-05 13:37:33 -0500

    add/ country name as property

--- src/utils/favoriteCities.js
+++ src/utils/favoriteCities.js
@@ -1,16 +1,19 @@
 const favoriteCities = [
   {
     city: 'San Francisco',
+    country: 'US',
     lat: 37.7749,
     lon: -122.4194,
   },
   {
     city: 'Tokyo',
+    country: 'JP',
     lat: 35.6895,
     lon: 139.6917,
   },
   {
     city: 'Madrid',
+    country: 'ES',
     lat: 40.4165,
     lon: -3.7026,
   },
