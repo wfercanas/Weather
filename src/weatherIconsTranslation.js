bbbe4f7fed95ce0f9c9dd86955682f237cbeeb63
wfercanas <fernando.canas@outlook.com>
2021-06-04 19:11:10 -0500

    add/ translate weather API icons to project icons

---
+++ src/weatherIconsTranslation.js
@@ -0,0 +1,23 @@
+/* Convert weather API icons to this projects icons nomenclature */
+const weatherIconsTranslation = {
+  I01d: '1',
+  I02d: '3',
+  I03d: '5',
+  I04d: '%',
+  I09d: '7',
+  I10d: '8',
+  I11d: '&',
+  I13d: '$',
+  I50d: 'M',
+  I01n: '2',
+  I02n: '4',
+  I03n: '5',
+  I04n: '%',
+  I09n: '7',
+  I10n: '8',
+  I11n: '&',
+  I13n: '$',
+  I50n: 'M',
+};
+
+export { weatherIconsTranslation };
