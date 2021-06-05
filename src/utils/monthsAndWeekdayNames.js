521a0c2b9836803e04194b2c22df165fa9f72663
wfercanas <fernando.canas@outlook.com>
2021-06-04 23:05:13 -0500

    add/ arrays for months and weekday names

---
+++ src/utils/monthsAndWeekdayNames.js
@@ -0,0 +1,26 @@
+const monthsOfTheYear = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const dayweekNames = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+export { monthsOfTheYear, dayweekNames };
