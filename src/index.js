4462be5bc0543a2ccb18df97f385cf9365a37112
wfercanas <fernando.canas@outlook.com>
2021-06-04 22:00:29 -0500

    remove/ move hourlyWeather func to new file

--- src/index.js
+++ src/index.js
@@ -1,47 +1,13 @@
 import { coordinatesURL } from './utils/API.js';
 import { favoriteCities } from './utils/favoriteCities.js';
-import { weatherIconsTranslation } from './utils/weatherIconsTranslation.js';
-
-/* DOM Elements */
-import { currentHourlyValues } from './utils/DOMElements.js';
 
 /* Functions */
 import { setCurrentCityWeather } from './functions/setCurrentCityWeather.js';
 import { setCurrentCityIndicators } from './functions/setCurrentCityIndicators.js';
+import { setCurrentCityHourlyWeather } from './functions/setCurrentCityHourlyWeather.js';
 
 let selectedCityIndex = 0;
 const selectedCity = favoriteCities[selectedCityIndex];
-
-/* Create hourly weather */
-const setCurrentCityHourlyWeather = (cityWeather) => {
-  for (let i = 0; i < 24; i++) {
-    const hourBlock = document.createElement('div');
-    const hour = document.createElement('p');
-    const weatherIcon = document.createElement('i');
-    const temp = document.createElement('p');
-
-    hourBlock.classList.add('hourly-data__content--block');
-    hour.classList.add('hourly-data__content--hour');
-    weatherIcon.classList.add('hourly-data__content--icon');
-    weatherIcon.classList.add('icon');
-    temp.classList.add('hourly-data__content--temp');
-
-    hour.textContent = `${
-      timestampToCityTime(cityWeather.hourly[i].dt, cityWeather.timezone).hour
-    }:${
-      timestampToCityTime(cityWeather.hourly[i].dt, cityWeather.timezone)
-        .minutes
-    }`;
-    weatherIcon.setAttribute(
-      'data-icon',
-      weatherIconsTranslation[`I${cityWeather.hourly[i].weather[0].icon}`]
-    );
-    temp.textContent = `${Math.round(cityWeather.hourly[i].temp)}°`;
-
-    hourBlock.append(hour, weatherIcon, temp);
-    currentHourlyValues.appendChild(hourBlock);
-  }
-};
 
 /* Fetch API */
 window
