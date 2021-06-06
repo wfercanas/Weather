f2aa212ea001239d46984b160ea836c19c355fc6
wfercanas <fernando.canas@outlook.com>
2021-06-06 18:22:10 -0500

    add/ turn on and off favorite button

---
+++ src/functions/setFavoriteButton.js
@@ -0,0 +1,19 @@
+/* Utils */
+import { favoriteButton } from '../utils/DOMElements.js';
+import { favoriteCities } from '../utils/favoriteCities.js';
+
+const setFavoriteButton = (city) => {
+  console.log(city);
+  if (favoriteButton.classList.contains('on')) {
+    favoriteButton.classList.remove('on');
+    favoriteButton.classList.add('off');
+  }
+  favoriteCities.forEach((favoriteCity) => {
+    if (favoriteCity.city === city.city) {
+      favoriteButton.classList.remove('off');
+      favoriteButton.classList.add('on');
+    }
+  });
+};
+
+export { setFavoriteButton };
