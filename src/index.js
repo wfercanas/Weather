098ee9626eb867bff26f2aaa86d93cab467f59f6
wfercanas <fernando.canas@outlook.com>
2021-06-06 13:03:10 -0500

    add/ event listener for searching new city

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 /* Utils */
 import { favoriteCities } from './utils/favoriteCities.js';
-import { favoritesContainer } from './utils/DOMElements.js';
+import { favoritesContainer, searchBar } from './utils/DOMElements.js';
 
 /* Functions */
 import { fetchInitialCityWeather } from './functions/fetchInitialCityWeather.js';
 import { fetchNewCityWeather } from './functions/fetchNewCityWeather.js';
 import { getSelectedFavoriteCity } from './functions/getSelectedFavoriteCity.js';
+import { searchNewCity } from './functions/searchNewCity.js';
 
 /* Initial values */
 let selectedCityIndex = 0;
@@ -22,3 +23,9 @@ const changeSelectedCity = (event) => {
 /* Event Listeners */
 window.addEventListener('load', fetchInitialCityWeather(selectedCity));
 favoritesContainer.addEventListener('click', changeSelectedCity);
+searchBar.addEventListener('submit', searchNewCity);
+searchBar.addEventListener('keydown', (event) => {
+  if (event.keyCode === 13) {
+    searchNewCity(event);
+  }
+});
